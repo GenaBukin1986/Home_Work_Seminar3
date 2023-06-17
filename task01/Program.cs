710fd849aa2475b3e4764dd866ecd82c0618a04f
Nick_Bear <bear_76@mail.ru>
2023-06-17 19:32:23 +0300

    Домашнее задание 3 семинара

---
+++ task01/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+Console.Clear();
+Console.Write("Введите целое пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (Convert.ToString(num).Length == 5) // Проверка может быть и такой if (num / 10000 > 0 && num / 10000 < 10)
+{
+// первый способ при помощи строки
+Console.Write("Первый способ: ");
+string num1 = Convert.ToString(num);
+if (num1[0] == num1[4] && num1[1] == num1[3]) Console.WriteLine($"Число {num} является палиндромом");
+else Console.WriteLine($"Число {num} не является палиндромом");
+// второй способ при помощи математических действий
+Console.Write("Второй способ: ");
+int a = num / 10000;
+int b = num /1000 % 10;
+int c = num / 10 % 10;
+int d = num % 10;
+if (a == d && b == c) Console.WriteLine($"Число {num} является палиндромом");
+else Console.WriteLine($"Число {num} не является палиндромом");
+}else Console.Write("Вы ввели не пятизначное число!");
---
+++ task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Clear();
+Console.Write("Введите любое целое число: ");
+// первый способ цикл for
+int num = int.Parse(Console.ReadLine());
+Console.Write("Первый способ: ");
+for (int i = 1; i <= num; i++) Console.Write($"{Math.Pow(i,3)} ");
+Console.Write("\n");
+// второй способ при помощи массива 
+Console.Write("Второй способ: ");
+int[] list1 = new int[num];
+for (int i = 1; i <= num; i++ ) list1[i - 1] = i * i * i;
+for (int i = 0; i < num; i++) Console.Write($"{list1[i]} ");
+Console.Write("\n");
+// второй модифицированный при помощи библиотеки Math
+Console.Write("Третий способ: ");
+double[] list2 = new double[num];
+for (int i = 1; i <= num; i++) list2[i - 1] = Math.Pow(i, 3);
+for (int i = 0; i < num; i++) Console.Write($"{list2[i]} ");
